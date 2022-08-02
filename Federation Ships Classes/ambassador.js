e54e0941f3d5b05a2937101e86a5a7ed188170c2
Zane Dax <neptunium0237@gmail.com>
2022-08-02 16:06:48 -0600

    ambassador setup

---
+++ Federation Ships Classes/ambassador.js
@@ -0,0 +1,18 @@
+
+/* 
+    
+    AMBASSADOR CLASS CRUISERS
+
+*/
+
+let ambassador_class_ships = [
+    {
+        number: 'NCC-',
+        name: '',
+        builder: 'Spacedock, San Francisco, Earth',
+        laid_down: { year: '2320', month: '' },
+        launched: { year: '2324', month: '' },
+        commissioned: { year: '2324', month: '' },
+        status: 'Active'
+    },
+]
