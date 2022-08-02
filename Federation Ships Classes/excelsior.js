5ae5474f18321ad7cb801d897510d7a759ed6c83
Zane Dax <neptunium0237@gmail.com>
2022-08-02 15:27:03 -0600

    excelsior ships

---
+++ Federation Ships Classes/excelsior.js
@@ -0,0 +1,286 @@
+
+/*
+    EXCELSIOR CLASS CRUISER 
+
+    Note: a starship named Excelsior exists under the Constitutiopn Class sharship, NCC 1763
+
+
+*/
+
+
+
+
+let excelsior_class_ships = [
+    {
+        number: 'NCC-2000',
+        name: 'Excelsior',
+        builder: 'Spacedock, San Francisco, Earth',
+        laid_down: { year: '2280', month: 'April' },
+        launched: { year: '2285', month: 'May' },
+        commissioned: { year: '2287', month: 'October' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-2544',
+        name: 'Repulse',
+        builder: 'San Francisco Fleet Yards, Earth',
+        laid_down: { year: '2288', month: 'January' },
+        launched: { year: '2292', month: 'April' },
+        commissioned: { year: '2292', month: 'August' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-',
+        name: 'Roosevelt',
+        builder: 'Spacedock, San Francisco, Earth',
+        laid_down: { year: '2288', month: 'March' },
+        launched: { year: '2292', month: 'October' },
+        commissioned: { year: '2292', month: 'November' },
+        status: 'Lost'
+    },
+    {
+        number: 'NCC-1701-B',
+        name: 'Enterprise',
+        builder: 'San Francisco Fleet Yards, Earth',
+        laid_down: { year: '2297', month: 'June' },
+        launched: { year: '2301', month: 'January' },
+        commissioned: { year: '2301', month: 'April' },
+        status: 'Scrapped'
+    },
+    {
+        number: 'NCC-4598',
+        name: 'Fearless',
+        builder: 'San Francisco Fleet Yards, Earth',
+        laid_down: { year: '2305', month: 'March' },
+        launched: { year: '2309', month: 'April' },
+        commissioned: { year: '2309', month: 'August' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-13958',
+        name: 'Okinawa',
+        builder: 'Newport News Fleet Yards, Earth',
+        laid_down: { year: '2322', month: 'October' },
+        launched: { year: '2326', month: 'October' },
+        commissioned: { year: '2327', month: 'January' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-14232',
+        name: 'Berlin',
+        builder: 'San Francisco Fleet Yards, Earth',
+        laid_down: { year: '2323', month: 'January' },
+        launched: { year: '2326', month: 'December' },
+        commissioned: { year: '2327', month: 'February' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-14394',
+        name: 'Tecumseh',
+        builder: 'San Francisco Fleet Yards, Earth',
+        laid_down: { year: '2323', month: 'March' },
+        launched: { year: '2327', month: 'May' },
+        commissioned: { year: '2327', month: 'August' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-14427',
+        name: 'Lexington',
+        builder: 'Spacedock, Earth',
+        laid_down: { year: '2323', month: 'June' },
+        launched: { year: '2327', month: 'June' },
+        commissioned: { year: '2327', month: 'September' },
+        status: 'Lost'
+    },
+    {
+        number: 'NCC-18253',
+        name: 'Potemkin',
+        builder: 'Saint Petersburg Fleet Yards, Earth',
+        laid_down: { year: '2325', month: 'March' },
+        launched: { year: '2329', month: 'April' },
+        commissioned: { year: '2329', month: 'June' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-18317',
+        name: 'Adelaide',
+        builder: 'New Aberdeen Fleet Yards, Aldeberan',
+        laid_down: { year: '2325', month: 'November' },
+        launched: { year: '2329', month: 'November' },
+        commissioned: { year: '2330', month: 'February' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-18328',
+        name: 'Yamato',
+        builder: 'New Aberdeen Fleet Yards, Aldeberan',
+        laid_down: { year: '2325', month: 'November' },
+        launched: { year: '2330', month: 'January' },
+        commissioned: { year: '2330', month: 'March' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-18674',
+        name: 'Bismark',
+        builder: 'Avondale Group, Ferrata Docks, Rigellium, Rigel II',
+        laid_down: { year: '2326', month: 'January' },
+        launched: { year: '2330', month: 'February' },
+        commissioned: { year: '2330', month: 'April' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-19016',
+        name: 'Indianapolis',
+        builder: 'Axaanivus Cesleco Starcraft, Bedii Plains, Alpha Centauri V',
+        laid_down: { year: '2326', month: 'May' },
+        launched: { year: '2330', month: 'April' },
+        commissioned: { year: '2330', month: 'June' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-34098',
+        name: 'Clark',
+        builder: 'Axaanivus Cesleco Starcraft, Bedii Plains, Alpha Centauri V',
+        laid_down: { year: '2335', month: 'January' },
+        launched: { year: '2339', month: 'January' },
+        commissioned: { year: '2339', month: 'March' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-34099',
+        name: 'Livingston',
+        builder: 'Spacedock, San Francisco, Earth',
+        laid_down: { year: '2335', month: 'January' },
+        launched: { year: '2339', month: 'February' },
+        commissioned: { year: '2339', month: 'May' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-38907',
+        name: 'Intrepid',
+        builder: 'Avondale Group, Ferrata Docks, Rigellium, Rigel II',
+        laid_down: { year: '2337', month: 'September' },
+        launched: { year: '2341', month: 'August' },
+        commissioned: { year: '2341', month: 'November' },
+        status: 'Lost'
+    },
+    {
+        number: 'NCC-38955',
+        name: 'Crockett',
+        builder: 'Seskon Trella, Chagala, Tellar',
+        laid_down: { year: '2337', month: 'November' },
+        launched: { year: '2341', month: 'September' },
+        commissioned: { year: '2341', month: 'December' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-38997',
+        name: 'Malinche',
+        builder: 'Fleet Yards, Starbase 12',
+        laid_down: { year: '2337', month: 'December' },
+        launched: { year: '2342', month: 'February' },
+        commissioned: { year: '2342', month: 'April' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-40512',
+        name: 'Gorkon',
+        builder: 'Seskon Trella, Chagala, Tellar',
+        laid_down: { year: '2338', month: 'May' },
+        launched: { year: '2342', month: 'July' },
+        commissioned: { year: '2342', month: 'October' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-42136',
+        name: 'Cairo',
+        builder: 'Cosmadyne Yards, Boston',
+        laid_down: { year: '2338', month: 'July' },
+        launched: { year: '2342', month: 'November' },
+        commissioned: { year: '2343', month: 'January' },
+        status: 'Lost'
+    },
+    {
+        number: 'NCC-42285',
+        name: 'Charleston',
+        builder: 'Cosmadyne Yards, Boston',
+        laid_down: { year: '2338', month: 'August' },
+        launched: { year: '2342', month: 'November' },
+        commissioned: { year: '2343', month: 'January' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-42296',
+        name: 'Hood',
+        builder: 'Seskon Trella, Chagala, Tellar',
+        laid_down: { year: '2338', month: 'October' },
+        launched: { year: '2342', month: 'November' },
+        commissioned: { year: '2343', month: 'January' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-42768',
+        name: 'Lakota',
+        builder: 'Fleet Yards at Starbase 12',
+        laid_down: { year: '2339', month: 'January' },
+        launched: { year: '2343', month: 'January' },
+        commissioned: { year: '2343', month: 'March' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-42857',
+        name: 'Grissom',
+        builder: 'Fleet Yards at Starbase 47',
+        laid_down: { year: '2339', month: 'March' },
+        launched: { year: '2343', month: 'April' },
+        commissioned: { year: '2343', month: 'June' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-442995',
+        name: 'Al-Batani',
+        builder: 'Fleet Yards at Starbase 47',
+        laid_down: { year: '2339', month: 'June' },
+        launched: { year: '2343', month: 'August' },
+        commissioned: { year: '2343', month: 'October' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-43305',
+        name: 'Valley Forge',
+        builder: 'Fleet Yards at Starbase 67',
+        laid_down: { year: '2339', month: 'July' },
+        launched: { year: '2343', month: 'August' },
+        commissioned: { year: '2343', month: 'November' },
+        status: 'Lost'
+    },
+    {
+        number: 'NCC-50446',
+        name: 'Crazy Horse',
+        builder: 'Fleet Yards at Starbase 84',
+        laid_down: { year: '2344', month: 'February' },
+        launched: { year: '2349', month: 'April' },
+        commissioned: { year: '2349', month: 'June' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-52043',
+        name: 'Melbourne',
+        builder: 'Fleet Yards at Starbase 84',
+        laid_down: { year: '2349', month: 'December' },
+        launched: { year: '2354', month: 'December' },
+        commissioned: { year: '2356', month: 'February' },
+        status: 'Lost'
+    },
+
+
+
+
+
+
+
+]
+
+//excelsior_class_ships[3].name
+
+// console.log(excelsior_class_ships)
