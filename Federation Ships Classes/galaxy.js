fe3470641b5160e96d3ba9ee1fe4bfb553e89c83
Zane Dax <neptunium0237@gmail.com>
2022-08-03 13:31:10 -0600

    galaxy class ships

---
+++ Federation Ships Classes/galaxy.js
@@ -0,0 +1,67 @@
+
+/* 
+    GALAXY  CLASS - LARGE EXPLORATORY CRUISER
+*/
+
+let galaxy_class_ships = [
+    {
+        number: 'NCC-70637',
+        name: 'Galaxy',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2348', month: 'November' },
+        launched: { year: '2357', month: 'March' },
+        commissioned: { year: '2357', month: 'November' },
+        status: 'Converted'
+    },
+    {
+        number: 'NCC-71099',
+        name: 'Challenger',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2349', month: 'March' },
+        launched: { year: '2359', month: 'September' },
+        commissioned: { year: '2360', month: 'July' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-71807',
+        name: 'Yamato',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2350', month: 'November' },
+        launched: { year: '2358', month: 'February' },
+        commissioned: { year: '2362', month: 'January' },
+        status: 'Lost'
+    },
+    {
+        number: 'NCC-1701-D',
+        name: 'Enterprise',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2351', month: 'June' },
+        launched: { year: '2358', month: 'June' },
+        commissioned: { year: '2363', month: 'October' },
+        status: 'Lost'
+    },
+    {
+        number: 'NCC-71832',
+        name: 'Odyssey',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2352', month: 'January' },
+        launched: { year: '2362', month: 'February' },
+        commissioned: { year: '2362', month: 'September' },
+        status: 'Lost'
+    },
+    {
+        number: 'NCC-71854',
+        name: 'Venture',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2353', month: 'April' },
+        launched: { year: '2363', month: 'April' },
+        commissioned: { year: '2364', month: 'January' },
+        status: 'Active'
+    },
+]
+
+
+
+
+
+
