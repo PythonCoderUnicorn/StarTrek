87d257d107b8fecd95b877da7152000159821247
Zane Dax <neptunium0237@gmail.com>
2022-08-03 13:05:38 -0600

    niagara class ships

---
+++ Federation Ships Classes/niagara.js
@@ -0,0 +1,36 @@
+
+/* NIAGARA CLASS FAST CRUISER */
+
+
+let niagrara_class_ships = [
+    {
+        number: 'NCC-28472',
+        name: 'Niagrara',
+        builder: 'San Francisco Fleet Yards, Earth',
+        laid_down: { year: '2341', month: 'September' },
+        launched: { year: '2343', month: 'September' },
+        commissioned: { year: '2343', month: 'November' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-28473',
+        name: 'Wellington',
+        builder: 'San Francisco Fleet Yards, Earth',
+        laid_down: { year: '2341', month: 'November' },
+        launched: { year: '2343', month: 'October' },
+        commissioned: { year: '2343', month: 'November' },
+        status: 'Lost'
+    },
+    {
+        number: 'NCC-59804',
+        name: 'Princeton',
+        builder: 'San Francisco Fleet Yards, Earth',
+        laid_down: { year: '2343', month: 'August' },
+        launched: { year: '2346', month: 'May' },
+        commissioned: { year: '2347', month: 'July' },
+        status: 'Active'
+    },
+    
+]
+
+
