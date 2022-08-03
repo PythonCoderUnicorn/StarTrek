4731f6176c2b6cc43fe28f0f37dd055bb6800c6f
Zane Dax <neptunium0237@gmail.com>
2022-08-03 17:36:03 -0600

    akira class ships

---
+++ Federation Ships Classes/akira.js
@@ -0,0 +1,269 @@
+
+/*
+    AKIRA CLASS - BATTLECRUISER
+
+
+    Utopia Planitia Fleet Yards, Mars
+    Chandley Works, Caravalia, Mars
+
+
+*/
+
+let akira_class_ships = [
+
+    {
+        number: 'NCC-62497',
+        name: 'Akira',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2342', month: 'September' },
+        launched: { year: '2346', month: 'December' },
+        commissioned: { year: '2346 ', month: 'June ' },
+        status: 'Active'
+    },
+
+    {
+   
+        number: 'NCC-62498',
+        name: 'Macross',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2342', month: 'September' },
+        launched: { year: '2346', month: 'December ' },
+        commissioned: { year: '2346', month: 'June' },
+        status: 'Active'
+    },
+
+    { //    
+        number: 'NCC-62499',
+        name: 'Gundam',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2342', month: 'September ' },
+        launched: { year: '2346 ', month: 'December' },
+        commissioned: { year: '2346', month: 'June' },
+        status: 'Active'
+    },
+
+    {  
+        number: 'NCC-63293',
+        name: 'Rabin',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2343', month: 'March' },
+        launched: { year: '2347', month: 'June ' },
+        commissioned: { year: '2348', month: 'February' },
+        status: 'Lost'
+    },
+
+    {   
+        number: 'NCC-63352',
+        name: 'Kaneda',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2342', month: 'March' },
+        launched: { year: '2347', month: 'June ' },
+        commissioned: { year: '2348', month: 'February' },
+        status: 'Active'
+    },
+
+
+    { 
+        number: 'NCC-63385',
+        name: 'Timberwolf',
+        builder: 'Chandley Works, Caravalia, Mars',
+        laid_down: { year: '2343', month: 'April' },
+        launched: { year: '2347', month: 'July' },
+        commissioned: { year: '2348', month: 'March' },
+        status: 'Active'
+    },
+
+    { 
+        number: 'NCC-63542',
+        name: 'Galahad',
+        builder: 'Chandley Works, Caravalia, Mars',
+        laid_down: { year: '2343', month: 'May' },
+        launched: { year: '2347', month: 'August' },
+        commissioned: { year: '2348', month: 'March' },
+        status: 'Active'
+    },
+
+    {  
+        number: 'NCC-63543',
+        name: 'Panther City',
+        builder: 'Chandley Works, Caravalia, Mars',
+        laid_down: { year: '2342', month: 'May' },
+        launched: { year: '2347', month: 'August' },
+        commissioned: { year: '2348', month: 'March' },
+        status: 'Active'
+    },
+
+    { 
+        number: 'NCC-63544',
+        name: 'Arthur C Clarke',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2343', month: 'May' },
+        launched: { year: '2347', month: 'August' },
+        commissioned: { year: '2348', month: 'April' },
+        status: 'Active'
+    },
+
+    {  
+        number: 'NCC-63549',
+        name: 'Thunderchild',
+        builder: 'Chandley Works, Caravalia, Mars',
+        laid_down: { year: '2343', month: 'May' },
+        launched: { year: '2347', month: 'August' },
+        commissioned: { year: '2348', month: 'April' },
+        status: 'Active'
+    },
+    
+
+    { 
+        number: 'NCC-64550',
+        name: 'Lodoss',
+        builder: 'Chandley Works, Caravalia, Marss',
+        laid_down: { year: '2343', month: 'September' },
+        launched: { year: '2347', month: 'December' },
+        commissioned: { year: '2348', month: 'August' },
+        status: 'Active'
+    },
+
+    { 
+        number: 'NCC-64551',
+        name: 'Guyver',
+        builder: 'Chandley Works, Caravalia, Mars',
+        laid_down: { year: '2343', month: 'September' },
+        launched: { year: '2347', month: 'December' },
+        commissioned: { year: '2348', month: 'August' },
+        status: 'Lost'
+    },
+
+    { 
+        number: 'NCC-64552',
+        name: 'Luputa',
+        builder: 'Chandley Works, Caravalia, Mars',
+        laid_down: { year: '2343', month: 'September' },
+        launched: { year: '2347', month: 'December' },
+        commissioned: { year: '2348', month: 'August' },
+        status: 'Active'
+    },
+
+    {  
+        number: 'NCC-64553',
+        name: 'Urusei Yatsura',
+        builder: 'Chandley Works, Caravalia, Mars',
+        laid_down: { year: '2343', month: 'September' },
+        launched: { year: '2347', month: 'December' },
+        commissioned: { year: '2348', month: 'August' },
+        status: 'Active'
+    },
+
+    { 
+        number: 'NCC-64646',
+        name: 'Appleseed',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2343', month: 'October' },
+        launched: { year: '2348', month: 'January' },
+        commissioned: { year: '2348', month: 'September' },
+        status: 'Active'
+    },
+
+    {  
+        number: 'NCC-65549',
+        name: 'Spector',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2344', month: 'October' },
+        launched: { year: '2349', month: 'January' },
+        commissioned: { year: '2349', month: 'September' },
+        status: 'Active'
+    },
+
+    {
+        number: 'NCC-66704',
+        name: 'Nausicaa',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2345', month: 'October' },
+        launched: { year: '2350', month: 'February' },
+        commissioned: { year: '2350', month: 'September' },
+        status: 'Active'
+    },
+
+    {
+        number: 'NCC-66724',
+        name: 'Dominion',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2345', month: 'October' },
+        launched: { year: '2350', month: 'January' },
+        commissioned: { year: '2350', month: 'September' },
+        status: 'Lost'
+    },
+
+    {
+        number: 'NCC-68322',
+        name: 'Justy',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2346', month: 'October' },
+        launched: { year: '2351', month: 'January' },
+        commissioned: { year: '2351', month: 'July' },
+        status: 'Active'
+    },
+
+    {
+        number: 'NCC-68553',
+        name: 'Buskirk',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2346', month: 'Deccember' },
+        launched: { year: '2351', month: 'March' },
+        commissioned: { year: '2351', month: 'November' },
+        status: 'Active'
+    },
+
+    {
+        number: 'NCC-70054',
+        name: 'Iczer',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2348', month: 'May' },
+        launched: { year: '2352', month: 'August' },
+        commissioned: { year: '2353', month: 'April' },
+        status: 'Active'
+    },
+
+    {
+        number: 'NCC-72712',
+        name: 'Orguss',
+        builder: 'Chandley Works, Caravalia, Mars',
+        laid_down: { year: '2363', month: 'December' },
+        launched: { year: '2368', month: 'March' },
+        commissioned: { year: '2368', month: 'October' },
+        status: 'Active'
+    },
+
+    {
+        number: 'NCC-73518',
+        name: 'Hanneamise',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2364', month: 'August' },
+        launched: { year: '2369', month: 'November' },
+        commissioned: { year: '2370', month: 'June' },
+        status: 'Active'
+    },
+
+    {
+        number: 'NCC-74191',
+        name: 'Dancougar',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2366', month: 'June' },
+        launched: { year: '2370', month: 'September' },
+        commissioned: { year: '2371', month: 'April' },
+        status: 'Active'
+    },
+
+    {
+        number: 'NCC-74315',
+        name: 'Mylene',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2366', month: 'September' },
+        launched: { year: '2370', month: 'December' },
+        commissioned: { year: '2371', month: 'June' },
+        status: 'Active'
+    },
+]
+
+
+
