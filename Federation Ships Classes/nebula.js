35cc4f02a71d197f938f00a575096f8763f4d288
Zane Dax <neptunium0237@gmail.com>
2022-08-03 12:05:09 -0600

    nebula class ships

--- Federation Ships Classes/nebula.js
+++ Federation Ships Classes/nebula.js
@@ -1,5 +1,241 @@
 
 /*
     NEBULA CLASS LIGHT CRUISER 
+
+
+
+
+    Utopia Planitia Fleet Yards, Mars
+    Chiokis Starship Construction, Thelavor, Andor
+    San Francisco Fleet Yards, Earth
+    Shor Ta'kel, Central Docks, 40 Eridani
+    Avondale Group, Ferrata Docks, Rigel II
+    Cosmadyne Yards, Boston
+    Puget Sound Fleet Yards
+
+    December    April   July    May     September   October     February
 */
+
+
+let nebulas_class_ships = [
+    {
+        number: 'NCC-60590',
+        name: 'Nebula',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2341', month: 'December' },
+        launched: { year: '2346', month: 'December' },
+        commissioned: { year: '2347', month: 'July' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-60591',
+        name: 'Farragut',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2341', month: 'December' },
+        launched: { year: '2346', month: 'December' },
+        commissioned: { year: '2347', month: 'July' },
+        status: 'Lost'
+    },
+    {
+        number: 'NCC-61826',
+        name: 'Monitor',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2342', month: 'April' },
+        launched: { year: '2350', month: 'June' },
+        commissioned: { year: '2351', month: 'March' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-61827',
+        name: 'Merrimac',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2342', month: 'April' },
+        launched: { year: '2350', month: 'June' },
+        commissioned: { year: '2351', month: 'March' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-61829',
+        name: 'Ronald E McNair',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2342', month: 'April' },
+        launched: { year: '2350', month: 'June' },
+        commissioned: { year: '2351', month: 'March' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-618832',
+        name: 'Lexington',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2342', month: 'April' },
+        launched: { year: '2350', month: 'July' },
+        commissioned: { year: '2351', month: 'March' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-61896',
+        name: 'Athena',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2342', month: 'May' },
+        launched: { year: '2350', month: 'July' },
+        commissioned: { year: '2351', month: 'March' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-61952',
+        name: 'Proxima',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2342', month: 'June' },
+        launched: { year: '2350', month: 'August' },
+        commissioned: { year: '2351', month: 'March' },
+        status: 'Lost'
+    },
+    {
+        number: 'NCC-62005',
+        name: 'Honshu',
+        builder: 'Chiokis Starship Construction, Thelavor, Andor',
+        laid_down: { year: '2342', month: 'June' },
+        launched: { year: '2350', month: 'August' },
+        commissioned: { year: '2351', month: 'April' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-62006',
+        name: 'Hera',
+        builder: 'Chiokis Starship Construction, Thelavor, Andor',
+        laid_down: { year: '2342', month: 'June' },
+        launched: { year: '2350', month: 'August' },
+        commissioned: { year: '2351', month: 'April' },
+        status: 'Lost'
+    },
+    {
+        number: 'NCC-62048',
+        name: 'Bellerephon',
+        builder: 'Chiokis Starship Construction, Thelavor, Andor',
+        laid_down: { year: '2342', month: 'June' },
+        launched: { year: '2350', month: 'August' },
+        commissioned: { year: '2351', month: 'May' },
+        status: 'Lost'
+    },
+    {
+        number: 'NCC-65420',
+        name: 'Phoenix',
+        builder: 'San Francisco Fleet Yards, Earth',
+        laid_down: { year: '2344', month: 'August' },
+        launched: { year: '2352', month: 'October' },
+        commissioned: { year: '2353', month: 'April' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-65421',
+        name: 'Intangible',
+        builder: "Shor Ta'kel, Central Docks, 40 Eridani",
+        laid_down: { year: '2344', month: 'August' },
+        launched: { year: '2352', month: 'October' },
+        commissioned: { year: '2353', month: 'April' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-65462',
+        name: 'Lone Wolf',
+        builder: "Shor Ta'kel, Central Docks, 40 Eridani",
+        laid_down: { year: '2344', month: 'September' },
+        launched: { year: '2352', month: 'November' },
+        commissioned: { year: '2353', month: 'April' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-65826',
+        name: 'Phantom',
+        builder: 'Avondale Group, Ferrata Docks, Rigel II',
+        laid_down: { year: '2344', month: 'November' },
+        launched: { year: '2353', month: 'January' },
+        commissioned: { year: '2353', month: 'June' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-65897',
+        name: 'Tripoli',
+        builder: 'Avondale Group, Ferrata Docks, Rigel II',
+        laid_down: { year: '2344', month: 'December' },
+        launched: { year: '2353', month: 'February' },
+        commissioned: { year: '2353', month: 'July' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-66808',
+        name: 'Ulysses',
+        builder: 'San Francisco Fleet Yards, Earth',
+        laid_down: { year: '2345', month: 'November' },
+        launched: { year: '2354', month: 'January' },
+        commissioned: { year: '2354', month: 'May' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-70352',
+        name: 'Leeds',
+        builder: 'San Francisco Fleet Yards, Earth',
+        laid_down: { year: '2348', month: 'August' },
+        launched: { year: '2356', month: 'October' },
+        commissioned: { year: '2357', month: 'April' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-70915',
+        name: "T'Kumbra",
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2349', month: 'February' },
+        launched: { year: '2357', month: 'April' },
+        commissioned: { year: '2357', month: 'November' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-71201',
+        name: 'Prometherus',
+        builder: 'Cosmadyne Yards, Boston',
+        laid_down: { year: '2349', month: 'June' },
+        launched: { year: '2357', month: 'August' },
+        commissioned: { year: '2358', month: 'March' },
+        status: 'Lost'
+    },
+    {
+        number: 'NCC-71805',
+        name: 'Endeavour',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2349', month: 'December' },
+        launched: { year: '2358', month: 'March' },
+        commissioned: { year: '2358', month: 'September' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-71906',
+        name: 'Khitomer',
+        builder: 'Puget Sound Fleet Yards, Earth',
+        laid_down: { year: '2356', month: 'October' },
+        launched: { year: '2364', month: 'December' },
+        commissioned: { year: '2365', month: 'June' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-71968',
+        name: 'Northstar',
+        builder: 'Puget Sound Fleet Yards, Earth',
+        laid_down: { year: '2357', month: 'October' },
+        launched: { year: '2366', month: 'January' },
+        commissioned: { year: '2365', month: 'June' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-72015',
+        name: 'Sutherland',
+        builder: 'San Francisco Fleet Yards, Earth',
+        laid_down: { year: '2359', month: 'December' },
+        launched: { year: '2368', month: 'March' },
+        commissioned: { year: '2368', month: 'October' },
+        status: 'Active'
+    },
+   
+]
+
+
 
