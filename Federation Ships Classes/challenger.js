6ea2793d42163f12e6573bd3d43b8a2a01eb2a66
Zane Dax <neptunium0237@gmail.com>
2022-08-03 15:05:15 -0600

    challenger ships

---
+++ Federation Ships Classes/challenger.js
@@ -0,0 +1,50 @@
+
+/*
+    CHALLENGER CLASS - LIGHT CRUISER
+
+*/
+
+let challenger_class_ships = [
+    {
+        number: 'NCC-57528',
+        name: 'Challenger',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2340', month: 'October' },
+        launched: { year: '2343', month: 'December' },
+        commissioned: { year: '2344', month: 'April' },
+        status: 'Lost'
+    },
+    {
+        number: 'NCC-57537',
+        name: 'Armstrong',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2340', month: 'November' },
+        launched: { year: '2344', month: 'January' },
+        commissioned: { year: '2344', month: 'May' },
+        status: 'Lost'
+    },
+    {
+        number: 'NCC-57566',
+        name: 'Kearsarge',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2340', month: 'December' },
+        launched: { year: '2344', month: 'January' },
+        commissioned: { year: '2344', month: 'June' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-57580',
+        name: 'Buran',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2341', month: 'January' },
+        launched: { year: '2344', month: 'March' },
+        commissioned: { year: '2344', month: 'August' },
+        status: 'Active'
+    },
+]
+
+
+
+
+
+
