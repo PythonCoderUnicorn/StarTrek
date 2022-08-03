efa6d8007739275f8a6ceb879594f2a22eec2184
Zane Dax <neptunium0237@gmail.com>
2022-08-03 10:02:14 -0600

    ambassador class ships

--- Federation Ships Classes/ambassador.js
+++ Federation Ships Classes/ambassador.js
@@ -7,12 +7,167 @@
 
 let ambassador_class_ships = [
     {
-        number: 'NCC-',
-        name: '',
+        number: 'NCC-10521',
+        name: 'Ambassador',
         builder: 'Spacedock, San Francisco, Earth',
+        laid_down: { year: '2320', month: 'April' },
+        launched: { year: '2324', month: 'June' },
+        commissioned: { year: '2324', month: 'December' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-1701-C',
+        name: 'Enterprise',
+        builder: 'San Francisco Fleet Yards, Earth',
+        laid_down: { year: '2321', month: 'January' },
+        launched: { year: '2325', month: 'April' },
+        commissioned: { year: '2325', month: 'August' },
+        status: 'Lost'
+    },
+    {
+        number: 'NCC-10794',
+        name: 'Maat',
+        builder: 'Saint Petersburg Fleet Yards, Earth',
+        laid_down: { year: '2321', month: 'March' },
+        launched: { year: '2325', month: 'June' },
+        commissioned: { year: '2325', month: 'November' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-12847',
+        name: 'Nightshade',
+        builder: 'San Francisco Fleet Yards, Earth',
+        laid_down: { year: '2321', month: 'June' },
+        launched: { year: '2325', month: 'August' },
+        commissioned: { year: '2326', month: 'February' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-12934',
+        name: 'Horatio',
+        builder: 'San Francisco Fleet Yards, Earth',
+        laid_down: { year: '2325', month: 'July' },
+        launched: { year: '2331', month: 'September' },
+        commissioned: { year: '2332', month: 'March' },
+        status: 'Lost'
+    },
+    {
+        number: 'NCC-13004',
+        name: 'Starseeker',
+        builder: 'Spacedock, San Francisco, Earth',
+        laid_down: { year: '2325', month: 'July' },
+        launched: { year: '2331', month: 'September' },
+        commissioned: { year: '2332', month: 'March' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-13011',
+        name: 'Highlander',
+        builder: 'San Francisco Fleet Yards, Earth',
+        laid_down: { year: '2325', month: 'July' },
+        launched: { year: '2331', month: 'September' },
+        commissioned: { year: '2332', month: 'March' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-13519',
+        name: 'Bonaventure',
+        builder: 'San Francisco Fleet Yards, Earth',
         laid_down: { year: '2320', month: '' },
         launched: { year: '2324', month: '' },
         commissioned: { year: '2324', month: '' },
         status: 'Active'
     },
+    {
+        number: 'NCC-26133',
+        name: 'Winter Solstice',
+        builder: 'Saint Petersburg Fleet Yards, Earth',
+        laid_down: { year: '2330', month: 'October' },
+        launched: { year: '2334', month: 'May' },
+        commissioned: { year: '2335', month: 'September' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-26136',
+        name: 'Zhukov',
+        builder: 'Saint Petersburg Fleet Yards, Earth',
+        laid_down: { year: '2330', month: 'November' },
+        launched: { year: '2334', month: 'June' },
+        commissioned: { year: '2335', month: 'December' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-26198',
+        name: 'Valdemar',
+        builder: 'Pederson Spaceport, Copenhagen, Earth',
+        laid_down: { year: '2331', month: 'February' },
+        launched: { year: '2334', month: 'September' },
+        commissioned: { year: '2335', month: 'December' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-26510',
+        name: 'Yamaguchi',
+        builder: 'Puget Sound Fleet Yards, Earth',
+        laid_down: { year: '2332', month: 'October' },
+        launched: { year: '2335', month: 'April' },
+        commissioned: { year: '2335', month: 'July' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-26513',
+        name: 'Exeter',
+        builder: 'Alfras Fleet Yards, Denab V',
+        laid_down: { year: '2332', month: 'January' },
+        launched: { year: '2335', month: 'October' },
+        commissioned: { year: '2336', month: 'January' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-26517',
+        name: 'Excalibur',
+        builder: 'Puget Sound Fleet Yards, Earth',
+        laid_down: { year: '2332', month: 'November' },
+        launched: { year: '2336', month: 'June' },
+        commissioned: { year: '2336', month: 'August' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-26632',
+        name: 'Gandhi',
+        builder: 'Alfras Fleet Yards, Denab V',
+        laid_down: { year: '2333', month: 'January' },
+        launched: { year: '2336', month: 'October' },
+        commissioned: { year: '2337', month: 'January' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-26849',
+        name: 'Adelphi',
+        builder: 'New Aberdeen Fleet Yards, Aldeberan',
+        laid_down: { year: '2333', month: 'March' },
+        launched: { year: '2337', month: 'January' },
+        commissioned: { year: '2337', month: 'May' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-28472',
+        name: 'Niagara',
+        builder: 'Spacedock, San Francisco, Earth',
+        laid_down: { year: '2334', month: 'September' },
+        launched: { year: '2337', month: 'November' },
+        commissioned: { year: '2338', month: 'February' },
+        status: 'Converted'
+    },
+    {
+        number: 'NCC-28473',
+        name: 'Wellington',
+        builder: 'Spacedock, San Francisco, Earth',
+        laid_down: { year: '2334', month: 'September' },
+        launched: { year: '2337', month: 'December' },
+        commissioned: { year: '2338', month: 'February' },
+        status: 'Converted'
+    },
 ]
+
+
