88e305984f5acebf63680c06b9af2d66cd36e471
Zane Dax <neptunium0237@gmail.com>
2022-08-04 10:24:24 -0600

    galaxy2 ships

---
+++ Federation Ships Classes/galaxy2.js
@@ -0,0 +1,75 @@
+
+/*
+    GALAXY II CLASS - LARGE EXPLORATORY CRUISER
+
+*/
+
+let galaxy2_class_ships = [
+    {
+        number: 'NCC-71867',
+        name: 'Trinculo',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2354', month: 'August' },
+        launched: { year: '2371', month: 'December' },
+        commissioned: { year: '2372 ', month: 'June ' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-71875',
+        name: 'Bright Star',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2355', month: 'September' },
+        launched: { year: '2370', month: 'January' },
+        commissioned: { year: '2370 ', month: 'May ' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-72069',
+        name: 'Werner von Braun',
+        builder: 'San Francisco Fleet Yards, Earth',
+        laid_down: { year: '2360', month: 'February' },
+        launched: { year: '2372', month: 'August' },
+        commissioned: { year: '2373 ', month: 'March ' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-72382',
+        name: 'Magellan',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2361', month: 'September' },
+        launched: { year: '2373', month: 'May' },
+        commissioned: { year: '2373 ', month: 'December ' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-77983',
+        name: 'Breedlove',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2371', month: 'March' },
+        launched: { year: '2374', month: 'April' },
+        commissioned: { year: '2378 ', month: 'April ' },
+        status: 'Trials'
+    },
+    {
+        number: 'NCC-78012',
+        name: 'Cortez',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2371', month: 'April' },
+        launched: { year: '2374', month: 'April' },
+        commissioned: { year: ' ', month: ' ' },
+        status: 'Lost'
+    },
+    {
+        number: 'NCC-70637',
+        name: 'Galaxy',
+        builder: 'Utopia Planitia Fleet Yards, Mars',
+        laid_down: { year: '2375', month: 'July' },
+        launched: { year: '2377', month: 'April' },
+        commissioned: { year: '2377 ', month: 'November ' },
+        status: 'Active'
+    },
+]
+
+
+
+
