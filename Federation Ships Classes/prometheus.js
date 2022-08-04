f4f29cd7dca83fca2c4728c4221350a5a720e818
Zane Dax <neptunium0237@gmail.com>
2022-08-04 12:34:24 -0600

    prometheus ship

---
+++ Federation Ships Classes/prometheus.js
@@ -0,0 +1,19 @@
+
+/*
+    PROMETHEUS CLASS - TACTICAL CRUISER
+
+*/
+
+
+let prometheus_class_ships = [
+    {
+        number: 'NCC-74913',
+        name: 'Prometheus',
+        builder: 'Fleet Yards, Deep Space 5',
+        laid_down: { year: '2367', month: 'June' },
+        launched: { year: '2374', month: 'April' },
+        commissioned: { year: '2376 ', month: 'December ' },
+        status: 'Active'
+    },
+]
+
