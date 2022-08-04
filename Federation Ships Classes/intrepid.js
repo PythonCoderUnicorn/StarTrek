b61834611c68f955858ec1cd6dd31b935bb1a426
Zane Dax <neptunium0237@gmail.com>
2022-08-04 11:09:51 -0600

    intrepid ships

---
+++ Federation Ships Classes/intrepid.js
@@ -0,0 +1,99 @@
+
+/*
+    INTREPID CLASS - CRUISER
+*/
+
+let intrepid_class_ships = [
+    {
+        number: 'NCC-74600',
+        name: 'Intrepid',
+        builder: 'Earth Station McKinley, Earth',
+        laid_down: { year: '2366', month: 'December' },
+        launched: { year: '2370', month: 'February' },
+        commissioned: { year: '2371 ', month: 'May ' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-74656',
+        name: 'Voyager',
+        builder: 'Earth Station McKinley, Earth',
+        laid_down: { year: '2367', month: 'March' },
+        launched: { year: '2370', month: 'May' },
+        commissioned: { year: '2371 ', month: 'September ' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-74657',
+        name: 'Bennu',
+        builder: 'Earth Station McKinley, Earth',
+        laid_down: { year: '2367', month: 'April' },
+        launched: { year: '2370', month: 'July' },
+        commissioned: { year: '2371 ', month: 'January ' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-74658',
+        name: 'Valkyrie',
+        builder: 'Earth Station McKinley, Earth',
+        laid_down: { year: '2367', month: 'April' },
+        launched: { year: '2370', month: 'July' },
+        commissioned: { year: '2371 ', month: 'February ' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-74791',
+        name: 'Destiny',
+        builder: 'Earth Station McKinley, Earth',
+        laid_down: { year: '2367', month: 'May' },
+        launched: { year: '2370', month: 'August' },
+        commissioned: { year: '2371 ', month: 'March ' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-74805',
+        name: 'Bellerophon',
+        builder: 'Earth Station McKinley, Earth',
+        laid_down: { year: '2367', month: 'May' },
+        launched: { year: '2370', month: 'August' },
+        commissioned: { year: '2371 ', month: 'March ' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-75127',
+        name: 'Morgan',
+        builder: 'Earth Station McKinley, Earth',
+        laid_down: { year: '2367', month: 'September' },
+        launched: { year: '2370', month: 'December' },
+        commissioned: { year: '2371 ', month: 'July ' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-75812',
+        name: 'Traveller',
+        builder: 'Earth Station McKinley, Earth',
+        laid_down: { year: '2369', month: 'March' },
+        launched: { year: '2371', month: 'June' },
+        commissioned: { year: '2371 ', month: 'November ' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-76254',
+        name: 'Tiger',
+        builder: 'Earth Station McKinley, Earth',
+        laid_down: { year: '2369', month: 'August' },
+        launched: { year: '2371', month: 'November' },
+        commissioned: { year: '2372 ', month: 'December ' },
+        status: 'Active'
+    },
+    {
+        number: 'NCC-76893',
+        name: 'Raptor',
+        builder: 'Earth Station McKinley, Earth',
+        laid_down: { year: '2370', month: 'March' },
+        launched: { year: '2373', month: 'June' },
+        commissioned: { year: '2373 ', month: 'December ' },
+        status: 'Active'
+    },
+]
+
+
