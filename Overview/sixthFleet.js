56af3bbffce5bdc6f63a727b0b7b8f8be14fce07
Zane Dax <neptunium0237@gmail.com>
2022-08-05 12:51:07 -0600

    6th fleet

---
+++ Overview/sixthFleet.js
@@ -0,0 +1,47 @@
+
+/*
+    SIXTH FLEET
+*/
+
+let sixth_fleet = [
+    {
+        location: 'Located near Deep Space 3',
+        patrol_area: 'Federation soace',
+        function: 'Patrol, defend and explore the Federation space.'
+    },
+
+    {
+        fleet_ships_strength: { ships: 107, TacFleet: 49, Exploration: 37, Auxillary: 21 },
+        fleet_commander: 'Admiral Pollock, Craig',
+        fleet_hq: 'Deep Space 3',
+        fleet_flagship: 'USS Cicero (NCC-75001)'
+    },
+
+    deployment = [
+        {
+            TaskForce: 'TaskForce 61',
+            Commander: 'Admiral Pollock, Craig',
+            Flagship: 'USS Cicero (NCC-75001)',
+            Commanding_Officer: 'Captain Zanari, Alec', 
+            Home_Port: 'Deep Space 3',
+            Deployment_Area: 'Coreward Exploration'
+        },
+        {
+            TaskForce: 'TaskForce 62',
+            Commander: 'Rear Admiral Amati, Giovanna',
+            Flagship: 'USS Leeds (CL 70352)',
+            Commanding_Officer: 'Captain Nasif, Estefano', 
+            Home_Port: 'Deep Space 5',
+            Deployment_Area: 'Coreward Exploration'
+        },
+        {
+            TaskForce: 'TaskForce 63',
+            Commander: 'Commodore Riseley-Pricthard, John',
+            Flagship: 'USS Thunderchild (CS 63549)',
+            Commanding_Officer: 'Captain Bakken, James', 
+            Home_Port: 'Deep Space 3',
+            Deployment_Area: 'Patrol'
+        },
+    ]
+
+]
