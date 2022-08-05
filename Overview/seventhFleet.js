d610993085fd1d08a5aa1d8291d3d1a4518c5959
Zane Dax <neptunium0237@gmail.com>
2022-08-05 14:31:57 -0600

    7th fleet

---
+++ Overview/seventhFleet.js
@@ -0,0 +1,76 @@
+
+/*
+    SEVENTH FLEET
+*/
+
+
+let seventh_fleet = [
+    {
+        location: 'Located at Spacedock',
+        patrol_area: 'Federation space',
+        function: 'Explore Federation space with First Fleet'
+    },
+
+    {
+        fleet_ships_strength: { ships: 289, TacFleet: 160, Exploration: 77, Auxillary: 52 },
+        fleet_commander: 'Admiral Jedrek, Lamia',
+        fleet_hq: 'Spacedock',
+        fleet_flagship: 'USS Sovereign (NCC-75000)'
+    },
+
+    deployment = [
+        {
+            TaskForce: 'TaskForce 71',
+            Commander: 'Admiral Jedrek, Lamia',
+            Flagship: 'USS Sovereign (NCC-75000)',
+            Commanding_Officer: 'Captain Washington, Beryl E', 
+            Home_Port: 'Starbase 10',
+            Deployment_Area: 'Romulan Neutral Zone'
+        },
+
+        {
+            TaskForce: 'TaskForce 72',
+            Commander: 'Rear Admiral Vosseller, Bob',
+            Flagship: 'USS Challenger (CKE 71099)',
+            Commanding_Officer: 'Rear Admiral Vosseller, Bob',
+            Home_Port: 'Starbase 185',
+            Deployment_Area: 'Coreward Exploration'
+        },
+
+        {
+            TaskForce: 'TaskForce 73',
+            Commander: 'Admiral Smith, Michael',
+            Flagship: 'USS Starlord (ES 74225)',
+            Commanding_Officer: 'Captain Podesta, Joe',
+            Home_Port: 'Deep Space 4',
+            Deployment_Area: 'Patrol'
+        },
+
+        {
+            TaskForce: 'TaskForce 74',
+            Commander: 'Commodore Hanford, Mark',
+            Flagship: 'USS Lexington (CL 61832)',
+            Commanding_Officer: 'Commodore Hanford, Mark',
+            Home_Port: 'Deep Space 10',
+            Deployment_Area: 'Patrol'
+        },
+
+        {
+            TaskForce: 'TaskForce 75',
+            Commander: 'Rear Admiral Owens, William',
+            Flagship: 'USS Excalibur (CH 26517)',
+            Commanding_Officer: 'Captain Buchman, Steve',
+            Home_Port: 'Deep Space 4',
+            Deployment_Area: 'Patrol'
+        },
+        {
+            TaskForce: 'TaskForce 76',
+            Commander: 'Vice Admiral Fokker, Roy',
+            Flagship: 'USS Macross (CS 62498)',
+            Commanding_Officer: 'Captain Grant. Claudia',
+            Home_Port: 'Deep Space 10',
+            Deployment_Area: 'Patrol'
+        },
+    ]
+
+]
