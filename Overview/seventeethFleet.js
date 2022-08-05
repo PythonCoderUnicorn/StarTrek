ed8135f5936341f3f660d72b4886f8a1e52ad092
Zane Dax <neptunium0237@gmail.com>
2022-08-05 17:09:03 -0600

    17th fleet

--- Overview/seventeethFleet.js
+++ Overview/seventeethFleet.js
@@ -3,15 +3,58 @@
 
 let seventeeth_fleet = [
     {
-        location: 'Located at Romulan Neutral Zone',
+        location: 'Located at rimward Federation space',
         patrol_area: 'Federation space',
-        function: 'Patrol and defends Federation space'
+        function: 'Patrol and explore Federation space'
     },
 
     {
-        fleet_ships_strength: { ships: 210, TacFleet: 147, Exploration: 40, Auxillary: 33 },
+        fleet_ships_strength: { ships: 187, TacFleet: 75, Exploration: 69, Auxillary: 43 },
         fleet_commander: 'Admiral Kharak Alsera',
-        fleet_hq: 'Starbase 343',
-        fleet_flagship: 'USS Galaxy (NCC-70637)'
+        fleet_hq: 'Starbase 17',
+        fleet_flagship: 'USS Venture (NCC-71854)'
     },
+
+    deployment = [
+        {
+            TaskForce: 'TaskForce 171',
+            Commander: 'Admiral Kharak Alsera',
+            Flagship: 'USS Venture (NCC-71854)',
+            Commanding_Officer: 'Captain Santavas, Miriya', 
+            Home_Port: 'Starbase 224',
+            Deployment_Area: 'Rimward Exploration'
+        },
+        {
+            TaskForce: 'TaskForce 172',
+            Commander: 'Rear Admiral Vokh, Kith',
+            Flagship: 'USS Laputa (CS 64552)',
+            Commanding_Officer: 'Captain Tauel, Sheeta', 
+            Home_Port: 'Camelot Station',
+            Deployment_Area: 'Patrol'
+        },
+        {
+            TaskForce: 'TaskForce 173',
+            Commander: 'Commodore Norris, Lillian',
+            Flagship: 'USS Adelphi (CH 26849)',
+            Commanding_Officer: 'Captain Othera, Mandar', 
+            Home_Port: 'Starbase 193',
+            Deployment_Area: 'Patrol'
+        },
+        {
+            TaskForce: 'TaskForce 174',
+            Commander: 'Captain Takahata, Isao',
+            Flagship: 'USS Traveller (CA 74716)',
+            Commanding_Officer: 'Captain Kethali, Sasha', 
+            Home_Port: 'Starbase 201',
+            Deployment_Area: 'Exploration'
+        },
+        {
+            TaskForce: 'TaskForce 175',
+            Commander: 'Commodore Sadeng, Kfrazz',
+            Flagship: 'USS Monitor (CL 61826)',
+            Commanding_Officer: 'Captain Blaine, Jarrod', 
+            Home_Port: 'Freedom III Station',
+            Deployment_Area: 'Rimward Exploration'
+        },
+    ]
 ]
