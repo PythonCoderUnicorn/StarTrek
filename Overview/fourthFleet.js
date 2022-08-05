503742c0bdb3d013268f4884fb6fd73adccbf28f
Zane Dax <neptunium0237@gmail.com>
2022-08-05 11:05:54 -0600

    4th fleet

---
+++ Overview/fourthFleet.js
@@ -0,0 +1,65 @@
+
+/*
+    FOURTH FLEET
+*/
+
+
+let fourth_fleet = [
+    {
+        location: 'Located in the southwestern quadrant edge of the Federation space',
+        patrol_area: 'Uncharted space',
+        function: 'Exploration.'
+    },
+
+    {
+        fleet_ships_strength: { ships: 204, TacFleet: 70, Exploration: 93, Auxillary: 41 },
+        fleet_commander: 'Admiral Rodriguez, Dinky',
+        fleet_hq: 'Cascade Station',
+        fleet_flagship: 'USS Magellon (NCC-72381)'
+    },
+
+    deployment = [
+        {
+            TaskForce: 'TaskForce 41',
+            Commander: 'Admiral Rodriguez, Dinky',
+            Flagship: 'USS Magellon (NCC-72381)',
+            Commanding_Officer: 'Captain Tuero, Esteban', 
+            Home_Port: 'Cascade Station',
+            Deployment_Area: 'Rimward Exploration'
+        },
+        {
+            TaskForce: 'TaskForce 42',
+            Commander: "Rear Admiral T'val",
+            Flagship: 'USS Zhukov (CH 23136)',
+            Commanding_Officer: 'Captain Zamora, Brian', 
+            Home_Port: 'Cascade Station',
+            Deployment_Area: 'Rimward Exploration'
+        },
+        {
+            TaskForce: 'TaskForce 43',
+            Commander: 'Commodore Shren',
+            Flagship: 'USS Bonaventure (CH 10542)',
+            Commanding_Officer: 'Captain Mswreth, Kathal', 
+            Home_Port: 'Starbase 269',
+            Deployment_Area: 'Patrol'
+        },
+        {
+            TaskForce: 'TaskForce 44',
+            Commander: 'Fleet Captain Piccolo, Fio',
+            Flagship: 'USS Bellerphon (CA 74705)',
+            Commanding_Officer: 'Captain Withers, Alan', 
+            Home_Port: 'Starbase 301',
+            Deployment_Area: 'Exploration'
+        },
+        {
+            TaskForce: 'TaskForce 45',
+            Commander: 'Rear Admiral Bunfold, Renee',
+            Flagship: 'USS Athena (CL 61896)',
+            Commanding_Officer: 'Captain Tobias, Goksel', 
+            Home_Port: "K'Ehyler Station",
+            Deployment_Area: 'Patrol'
+        },
+    ]
+
+
+]
