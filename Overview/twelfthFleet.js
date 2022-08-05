029dfe96c652c7e5c1da2c9913e1eac43fcb47d1
Zane Dax <neptunium0237@gmail.com>
2022-08-05 16:53:59 -0600

    12th fleet

---
+++ Overview/twelfthFleet.js
@@ -0,0 +1,71 @@
+
+/*
+    TWELFTH FLEET
+*/
+
+let twelfth_fleet = [
+    {
+        location: 'Located at Romulan Neutral Zone',
+        patrol_area: 'Federation space',
+        function: 'EPatrol and defends Federation space'
+    },
+
+    {
+        fleet_ships_strength: { ships: 210, TacFleet: 147, Exploration: 40, Auxillary: 33 },
+        fleet_commander: 'Admiral Kharak Alsera',
+        fleet_hq: 'Starbase 343',
+        fleet_flagship: 'USS Galaxy (NCC-70637)'
+    },
+
+    deployment = [
+        {
+            TaskForce: 'TaskForce 121',
+            Commander: 'Admiral Kharak Alsera',
+            Flagship: 'USS Galaxy (NCC-70637)',
+            Commanding_Officer: 'Captain Inokuma, Yawara', 
+            Home_Port: 'Starbase 343',
+            Deployment_Area: 'Exploration'
+        },
+        {
+            TaskForce: 'TaskForce 122',
+            Commander: 'Vice Admiral Anderson, William',
+            Flagship: 'USS Entente (DN 73280)',
+            Commanding_Officer: 'Captain Iczer, Kara', 
+            Home_Port: 'Starbase 343',
+            Deployment_Area: 'Romulan Neutral Zone'
+        },
+        {
+            TaskForce: 'TaskForce 123',
+            Commander: 'Rear Admiral Westfall, Robert',
+            Flagship: 'USS Krazny Oktyabr (BB 72201)',
+            Commanding_Officer: 'Rear Admiral Westfall, Robert',
+            Home_Port: 'Troubador Station',
+            Deployment_Area: 'Klingon Border'
+        },
+        {
+            TaskForce: 'TaskForce 124',
+            Commander: 'Rear Admiral Adama, Lorne',
+            Flagship: 'USS Battlestar (DN 23110)',
+            Commanding_Officer: 'Captain Crowl, David',
+            Home_Port: 'Outpost 4',
+            Deployment_Area: 'Romulan Neutral Zone'
+        },
+        {
+            TaskForce: 'TaskForce 125',
+            Commander: 'Rear Admiral Heath, David',
+            Flagship: 'USS Marko Ramius (DN 23102)',
+            Commanding_Officer: 'Captain Stewart, Donna',
+            Home_Port: 'Starbase 12',
+            Deployment_Area: 'Klingon Border'
+        },
+        {
+            TaskForce: 'TaskForce 126',
+            Commander: 'Commodore Corleigh, Seileach',
+            Flagship: 'USS Phoenix (CL 65420)',
+            Commanding_Officer: 'Commodore Corleigh, Seileach',
+            Home_Port: 'Troubador Station',
+            Deployment_Area: 'Klingon Border'
+        },
+
+    ]
+]
