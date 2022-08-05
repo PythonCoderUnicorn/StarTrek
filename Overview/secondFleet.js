527d4145798cf5a03c6f8881ab4e454f9c0acc95
Zane Dax <neptunium0237@gmail.com>
2022-08-05 10:46:24 -0600

    second fleet

---
+++ Overview/secondFleet.js
@@ -0,0 +1,63 @@
+
+/*
+    STARFLEET - SECODN FLEET
+*/
+
+
+let second_fleet = [
+    {
+        location: 'Located in the southern edge of the Federation in the Beta Quadrant',
+        patrol_area: ['Gorn Hegemony', "Tholian Assembly"],
+        function: 'Patrols and defends Federation territory.'
+    },
+
+    {
+        fleet_ships_strength: { ships: 241, TacFleet: 156, Exploration: 49, Auxillary: 36 },
+        fleet_commander: 'Admiral Rosenberg, Willow',
+        fleet_hq: 'Dark Silence Station',
+        fleet_flagship: 'USS Werner von Braun (NCC-72069)'
+    },
+
+    deployment = [
+        {
+            TaskForce: 'TaskForce 21',
+            Commander: 'Fleet Admiral Summers, Willow',
+            Flagship: 'Werner von Braun (CKE 72069)',
+            Commanding_Officer: 'Rear Admiral Boreanaz, Seth', 
+            Home_Port: 'Dark Silence Station',
+            Deployment_Area: 'Gorn Border'
+        },
+        {
+            TaskForce: 'TaskForce 22',
+            Commander: 'Fleet Captain Wampler, Gerri',
+            Flagship: 'Highlander (CH 10530)',
+            Commanding_Officer: 'Fleet Captain Wampler, Gerri',
+            Home_Port: 'Starbase 63',
+            Deployment_Area: 'Klingon Border'
+        },
+        {
+            TaskForce: 'TaskForce 23',
+            Commander: "Commodore M'reth",
+            Flagship: 'Valdemar (CH 26198)',
+            Commanding_Officer: 'Captain Pedersen, Danjette', 
+            Home_Port: 'Dark Silence Station',
+            Deployment_Area: 'Exploration'
+        },
+        {
+            TaskForce: 'TaskForce 24',
+            Commander: 'Commodore Amasawa, Seiji',
+            Flagship: 'Akira (CS 62497)',
+            Commanding_Officer: 'Captain Harada, Yuko', 
+            Home_Port: 'Starbase 251',
+            Deployment_Area: 'Tholian Border'
+        },
+        {
+            TaskForce: 'TaskForce 25',
+            Commander: 'Rear Admiral Pagott, Marco',
+            Flagship: 'Tripoli (CL 65897)',
+            Commanding_Officer: "Captain M'Giia", 
+            Home_Port: 'Chattahooche Station',
+            Deployment_Area: 'Patrol'
+        },
+    ]
+]
