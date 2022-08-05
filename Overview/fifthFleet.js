5d321eb8edfa90cd7f855f11035aef90bbc05c38
Zane Dax <neptunium0237@gmail.com>
2022-08-05 12:29:50 -0600

    fifth fleet

---
+++ Overview/fifthFleet.js
@@ -0,0 +1,55 @@
+
+/*
+    FIFTH FLEET
+*/
+
+let fifth_fleet = [
+    {
+        location: 'Located in Nexus Spacedock',
+        patrol_area: 'Klingon Empire & Cardassian Union',
+        function: 'Patrol, defend and explore the Federation space.'
+    },
+
+    {
+        fleet_ships_strength: { ships: 227, TacFleet: 121, Exploration: 58, Auxillary: 48 },
+        fleet_commander: 'Admiral Wallace, Chris',
+        fleet_hq: 'Nexus Spacedock, Galena, Obsidian System',
+        fleet_flagship: 'USS Bright Star (NCC-71875)'
+    },
+
+    deployment = [
+        {
+            TaskForce: 'TaskForce 51',
+            Commander: 'Admiral Wallace, Chris',
+            Flagship: 'USS Bright Star (NCC-71875)',
+            Commanding_Officer: 'Captain Miyazaki, Nausicaa', 
+            Home_Port: 'Spacedock Nexus',
+            Deployment_Area: 'Coreward Exploration'
+        },
+        {
+            TaskForce: 'TaskForce 52',
+            Commander: 'Rear Admiral Roithinger, Kurt',
+            Flagship: 'USS Destiny (CA 74691)',
+            Commanding_Officer: 'Captain Idell, Stephen', 
+            Home_Port: 'Starbase 514',
+            Deployment_Area: 'Patrol'
+        },
+        {
+            TaskForce: 'TaskForce 53',
+            Commander: 'Vice Admiral Campbell, Sharon',
+            Flagship: 'USS Pendragon (BB 2005)',
+            Commanding_Officer: 'Captain Halsey, Teri', 
+            Home_Port: 'Spacedock Nexus',
+            Deployment_Area: 'Cardassian Union Border'
+        },
+        {
+            TaskForce: 'Fifth MEU',
+            Commander: 'ALt. General Akers, Scott',
+            Flagship: 'USS Kensington (CH 75016)',
+            Commanding_Officer: 'Brigadier Hoff, Diana', 
+            Home_Port: 'Greywolf Station',
+            Deployment_Area: 'Cardassian Union Border'
+        },
+    ]
+
+]
