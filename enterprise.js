5f2e7d6e9e958624eb1efb37bd5b21eebedc11a8
Zane Dax <neptunium0237@gmail.com>
2022-08-17 13:55:10 -0600

    TNG

--- enterprise.js
+++ enterprise.js
@@ -5,8 +5,40 @@ const key_locations = {
     bridge_battle : 'Deck 8',
     bridge_main: 'Deck 1',
     brig: 'Deck 38',
-    
+    cargo_bays: 'Decks 4, 38 & 39',
+    docking_port_dorsal: 'Deck 25',
+    engineering_main: 'Deck 36',
+    gymnasium: 'Deck 11',
+    observation_lounge : 'Deck 1',
+    ready_room: 'Deck 1',
+    shuttlebay : {
+        main: 'Deck 4',
+        two: 'Deck 13',
+        three : 'Deck 13'
+    },
+    sickbay: 'Deck 12',
+    ten_forward: 'Deck 10',
+    transporter: {
+        rooms_1_4: 'Deck 6',
+        rooms_5_6 : 'Deck 14'
+    },
+
+    personal_quarters : {
+        Crusher_Beverly : 'Deck 7',
+        Crusher_Wesley : 'Deck 5',
+        LaForge_Geordi : 'Deck 4',
+        Picard_Jean_Luc : 'Deck 8',
+        Riker_Will : 'Deck 8',
+        Troi_Deanna : 'Deck 8',
+        Worf : 'Deck 5'
+    }
+
+
 }
+
+
+
+
 
 
 
@@ -23,6 +55,6 @@ function location(loc) {
     console.log('\n')
 }
 
-// console.log( key_locations.bridge_battle )
+// console.log( key_locations.personal_quarters.LaForge_Geordi )
 
 location('bridge' )
