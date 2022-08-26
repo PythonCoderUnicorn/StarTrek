04f21797e24447f0839764fd4ff50d0672d461d3
Zane Dax <neptunium0237@gmail.com>
2022-08-26 12:51:53 -0600

    phrases

---
+++ vulcan/phrases.js
@@ -0,0 +1,121 @@
+
+// phrases from Star Trek movies
+
+let phrases = [
+    {
+        "Dakh orfik-kel aushfamaluhr shaukaush fi'aifa mazhiv": 'Our ancestors cast out their animal passions here on these sands'
+    },
+    {
+        "Sha'koshtri korseiven bai'elkhrul-akteiben t'Kolinahr": 'Our race was saved by attainment of Kolinahr'
+    },
+    {
+        "Kolinahr, k'fai'el ek'zherka t'forti foshul": ' Kolinahr through wich all emotion is finally shed'
+    },
+    {
+        "Zup-tor vu akarshif - Spahkh": 'You have labored long Spock'
+    },
+    {
+        "I'poprah fasei setebuhk t'ovsot-olozhika": 'Now receive from us this symbol of total logic'
+    },
+    {
+        "Nahp hif-bi du throks":'Your thoughts, give them to me'
+    },
+    {
+        "Kashkau, Spahkh, wuhkuh eh teretuhr": 'Our minds, one and together'
+    },
+    {
+        "Tish hokni'es kwi'shoret":'This consciousness calling to you from space'
+    },
+    {
+        "Estuhl terrupik khaf Spahkh":'It touches your human blood, Spock'
+    },
+    {
+        "Du vravshal srashiv t'Kolinahr": 'You hvae not achieved Kolinahr'
+    },
+    {
+        "Ti kilko-srashiv kitok-wilat":'His answer lies elsewhere'
+    },
+    {
+        "I'tah tehrai k'etwel":'He will not achieve his goal with us'
+    },
+    {
+        "Dif-tor heh smusma, Spahkh":'Live long and prosper, Spock'
+    },
+    {
+        "Gishen worla ik-banu":'He is never what I expect'
+    },
+    {
+        "Wakli ak'wikman ,ot-lan?":'What surprises you, lieutenant?'
+    },
+    {
+        "Ish'veh ni komin":'That one is so human'
+    },
+    {
+        "Kling aklami buhfik, Saavik-kam":'Nobody is perfect, Saavik'
+    },
+    {
+        "Dom, ki'sariah":'So, it has come'
+    },
+    {
+        "Dungi tu sahrafel?":'Will you trust me?'
+    },
+    {
+        "Ben vahl navun":'Grant us success'
+    },
+    {
+        "Du ki'avon-telik?":'Are you hungry?'
+    },
+    {
+        "Isha nash-veh Vuhlkansu, pontal na'sochya":'T too am a Vulcan, bred to peace'
+    },
+    {
+        "Olozhika, palik t'kau ri shaht":'Logic is the beginning of wisdom not the end'
+    },
+    {
+        "Ri k'olozhika stau lof-fam":'It is illogical to kill without reason'
+    },
+    {
+        "Nar-tor tevahk, k'hen-tor kanok-ha'kiv sariah na'shaht lu haishan wak. Goh kuv ish ha'kiv vesh'thinoi la'tusa pak-tan t'ha'kiv": 'Accepting death, by understanding that every life comes to an end, when time demands it. Loss of life is to be mourned but only if the life was wasted'
+    },
+    {
+        "Pthak svi'zherka, dan-karik heh dan-khrashik":'Fear is among the strongest and most violent of the emotions'
+    },
+    {
+        "Svi wonil kash-ral talik Hassu, fi'tevahk ek yem-tor, ruhm kastik-goh-yokulsu": 'In a strict scientific sense Doctor, we all feed on death, even the vegetarians'
+    },
+    {
+        "Noshau u'dvinsular tum-vel ohrom heh yeht-urgam'es, nu ri aitlu ne'au dvin-tor": 'Computers make excellent and efficient servants, but I have no wish to serve under them'
+    },
+    {
+        "Tum-vellar weht yeht-urgam'es, ri weh-rom do kimihnsu":'Computers are more efficient, not better than human beings'
+    },
+    {
+        "Kup-fai-tor ri ruhm Vuhlkansu ri-Fainu":'Not even Vulcans can know the Unknown'
+    },
+    {
+        "Ma ri ni sanosh-yehat vah aitlu. Nam-tor ri k'olozhika hi k'ashiv yeht": 'Having is not so pleasing as wanting. It is not logical, but it is often true'
+    },
+    {
+        "Ma so-resh ri lof il utvau hi lau-ma tehrai":'Madness has no purpose or reason, but it may have a goal'
+    },
+    {
+        "Nam-tor zherka na'nash u'akanik, nam-tor u'ek'talsu":'Emotions are alien to me, I am a scientist'
+    },
+    {
+        "Spunau bolayalar t'Wehku bolayar t'Zamu il t'Veh":'The needs of the many outweigh the needs of the few or the one'
+    },
+    {
+        "Ki'nam-tor nash-veh heh kwon-sum dungau nam-tor t'hai'la t'du":'I have been and always shall be your friend'
+    },
+    {
+        "Wilat ri nam-tor zherka, ri nam-tor dvubolek na'khrash":'Where there is no emotion, there is no motive for violence'
+    },
+    {
+        "Nam-tor wahtayalar u'yeht wilat goh-pulayau zherka":'Insults are effective only where emotion is present'
+    }
+]
+
+
+let f = phrases[0]
+
+console.log( f )
